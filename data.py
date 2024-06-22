d14a9a6eac941c25f038156c5e2a868e458c1976
abhayonkar <abhayronkar@gmail.com>
2024-06-22 23:56:32 +0530

    pymo

--- data.py
+++ data.py
@@ -8,24 +8,22 @@ product_data = resp.json()
 
 print(product_data)
 
-# products = product_data['products']
+groceries = []
 
-# groceries = []
+for product in product_data:
+    if product.get('fall') == 'Fell':
+        groceries.append(product)
 
-# for product in products:
-#     if product['category'] == 'groceries':
-#         groceries.append(product)
-
-# try:
-#     client=MongoClient('mongodb://localhost:27017/')
-#     print("MongoDB Connection Successfully!")
-#     db=client['pymo1']
-#     col=db['product']
-#     col.insert_many(groceries)
-#     print("Data Inserted Successfully!")
-# except:
-#     print("Buddy Check -code one more time!")
-# finally:
-#     col=None
-#     db=None
-#     client=None
+try:
+    client=MongoClient('mongodb://localhost:27017/')
+    print("MongoDB Connection Successfully!")
+    db=client['pymo1']
+    col=db['product']
+    col.insert_many(groceries)
+    print("Data Inserted Successfully!")
+except:
+    print("Buddy Check -code one more time!")
+finally:
+    col=None
+    db=None
+    client=None
